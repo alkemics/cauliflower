7531747d2760a02de95a785bdae76c1d7f2c30d8
Clément Danjou <cdanjou@alkemics.com>
2015-10-29 17:55:03 +0100

    Fixing some obvious bugs

--- Cauliflower.js
+++ Cauliflower.js
@@ -1,46 +1,60 @@
 export default class Cauliflower {
-  constructor(host) {
-    this.config = config;
+
+  constructor() {
     this.patternCallbacks = [];
     this.simpleCallbacks = {};
     this.currentId = 0;
     this.plugins = {};
   }
+
+  addPlugin (plugin) {
+    this.plugins[plugin.name] = plugin;
+  }
+
+  removePlugin (name) {
+    this.plugins[name];
+  }
+
   on (pattern, handler) {
     var id = this.currentId++;
-    var handler = { id, pattern, callback };
+    var callback;
     if (typeof handler === 'string') {
       callback = plugins[handler].catch.bind(plugins[handler]);
     } else {
       callback = handler;
     }
+    var handler = { id, pattern, callback };
     if (pattern instanceof RegExp) {
       this.patternCallbacks.push(handler);
     } else if (typeof pattern === 'string') {
       if (this.simpleCallbacks[pattern] == null) {
-          this.simpleCallbacks = [];
+        this.simpleCallbacks[pattern] = [];
       }
-      this.simpleCallbacks.push(handler);
+      this.simpleCallbacks[pattern].push(handler);
     }
     return id;
   }
+
   off (id) {
     for (var key in this.simpleCallbacks) {
       this.simpleCallbacks[key].filter((handler) => {
         if (handler.id === id) return false;
         return true;
-      }
+      });
     }
     this.patternCallbacks.filter((handler) => {
       if (handler.id === id) return false;
       return true;
-    }
+    });
   }
+
   throw (identifier, ...args) {
+    console.log(this.simpleCallbacks);
     if (this.simpleCallbacks[identifier]) {
       this.simpleCallbacks[identifier].forEach((handler) => {
+        console.log(handler);
         handler.callback(...args);
-      }
+      });
     }
     if (this.patternCallbacks.length) {
       this.patternCallbacks.forEach((handler) => {
