d14fec5322fb48146ed87753a9f4f8c023c0c491
Clément Danjou <cdanjou@alkemics.com>
2015-11-02 19:01:51 +0100

    Fixing packaging

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _Cauliflower = require('./Cauliflower');
+
+var _Cauliflower2 = _interopRequireDefault(_Cauliflower);
+
+var _sentry = require('./sentry');
+
+var _sentry2 = _interopRequireDefault(_sentry);
+
+var cauliflower = new _Cauliflower2['default']();
+
+exports['default'] = cauliflower;
+module.exports = exports['default'];
