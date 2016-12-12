314da98ac01469278f2dc80a990a31d83aea7a60
Clément 'birdy' Danjou <clement.danjou@vinaigredemiel.com>
2016-12-12 16:00:48 +0100

    Handle native errors and custom objects

--- src/sentry.js
+++ src/sentry.js
@@ -1,10 +1,22 @@
 import Raven from 'raven-js';
 
+const isObject = (what) => (typeof what === 'object' && what !== null);
+
+const isError = (what) => (
+  isObject(what) &&
+  Object.prototype.toString.call(what) === '[object Error]' ||
+  what instanceof Error
+);
+
 var sentry = {
   name: 'sentry',
   catch: (name, e) => {
-    sentry.setExtraContext(name);
-    Raven.captureException(e);
+    if (isError(e)) {
+      Raven.captureException(e);
+    } else {
+      sentry.setExtraContext(e);
+      Raven.captureException(name);
+    }
   },
   setDSN: (dsn) => {
     Raven.config(dsn).install();
